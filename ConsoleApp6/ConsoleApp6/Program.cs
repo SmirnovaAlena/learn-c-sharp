4e92378301e7fc0620f2b9aa961ce7bae2fa2736
SmirnovaAlena <smirnova.alenad@gmail.com>
2024-05-28 20:03:38 +0300

    builderCake

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        public class Cake
+        {
+            public bool chocolate;
+            public bool cream;
+            public int k;
+            public int c;
+            public string client;
+            public void SetCh( bool b) { chocolate = b; }
+            public void SetCr(bool b) { cream = b; }
+            public void SetK(int k2) { k = k2; }
+            public void SetC(int k2) { c = k2; }
+            public void SetClient(string a) { client = a; }
+
+        }
+        public class Builder: Cake
+        {
+            private Cake c = new Cake();
+            public void SetCh(bool b) { chocolate = b; }
+            public void SetCr(bool b) { cream = b; }
+            public void SetK(int k2) { k = k2; }
+            public void SetC(int k3) { c = k3; }
+            public void SetClient(string a) { client = a; }
+            public 
+
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
